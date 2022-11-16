e71da449ef0c396e8994a5492c11017e58fc8f07
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2022-11-16 11:12:18 +0100

    Add BaseEntity

---
+++ VerhuringCandyWebsite.Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerhuringCandyWebsite.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime DateDeleted { get; set; }
+    }
+}
