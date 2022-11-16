5edcd8864cbc18215b0794b857cdcaec25665e29
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2022-11-16 11:12:49 +0100

    Add RentableItem

---
+++ VerhuringCandyWebsite.Core/Entities/RentableItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerhuringCandyWebsite.Core.Entities
+{
+    public class RentableItem : BaseEntity
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
